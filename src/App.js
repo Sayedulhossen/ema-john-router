790c665a8e7dd7c168a9dea12345feb052a1a138
Sayedulhossen <mdsarma999@gmail.com>
2021-12-25 05:46:33 +0600

    update

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import './App.css';
+import Header from './components/Header/Header';
+import Inventory from './components/Inventory.js/Inventory';
+import NotFound from './components/NotFound/NotFound';
+import OrderReview from './components/OrderReview/OrderReview';
+import Shop from './components/Shop/Shop';
+
+function App() {
+  return (
+    <div>
+      
+          <Router>
+          <Header></Header>
+            <Routes>
+              <Route path="/" element={<Shop/>}/>
+              <Route path="/shop" element={<Shop/>}/>
+              <Route path="/orders" element={<OrderReview/>}/>
+              <Route path="/inventory" element={<Inventory/>}/>
+              <Route path="*" element={<NotFound/>}/>
+            </Routes>
+          </Router>
+    </div>
+  );
+}
+
+export default App;
